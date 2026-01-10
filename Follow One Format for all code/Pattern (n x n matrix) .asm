b9b8132ec833c684ad6d527116e397cbb17996ce
Safayet Ullah <sojibmirzabd@gmail.com>
2026-01-11 00:23:54 +0600

    *

---
+++ Follow One Format for all code/Pattern (n x n matrix) .asm
@@ -0,0 +1,54 @@
+.model small
+.stack 100h
+
+.data
+   msg db "Enter Number:  $"
+
+.code
+main proc
+    mov ax, @data
+    mov ds, ax
+
+    lea dx, msg
+    mov ah, 9
+    int 21h
+
+    mov ah, 1
+    int 21h
+    sub al, '0'
+    mov bl, al   ; n columns   
+    mov cl, al   ; n rows
+    xor ch, ch   ; cx = n
+
+    ; new line
+    mov ah, 2
+    mov dl, 13
+    int 21h
+    mov dl, 10
+    int 21h
+
+outer_loop:
+        push cx
+        mov cl, bl   ; reload from BL (not AL)
+        xor ch, ch
+
+inner_loop:
+        mov dl, '*'
+        mov ah, 2
+        int 21h
+        loop inner_loop
+
+        mov ah, 2
+        mov dl, 13
+        int 21h
+        mov dl, 10
+        int 21h
+
+        pop cx
+        loop outer_loop
+
+exit:
+    mov ah, 4Ch
+    int 21h
+main endp
+end main
